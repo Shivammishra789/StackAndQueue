cbcaf737882c388b856c558f05a5806802cacde7
Shivammishra789 <shivammishra7789@gmail.com>
2021-11-01 00:40:08 +0530

    created stack and added 3 elements

---
+++ Stack.java
@@ -0,0 +1,15 @@
+package com.stackandqueue;
+
+import java.util.LinkedList;
+
+public class Stack {
+
+	LinkedList<Integer> list = new LinkedList<Integer>();
+
+	public void push(int data) {
+
+		list.add(data);  // adding new data to linkedlist
+		
+	}
+	
+}
---
+++ StackQueueMain.java
@@ -0,0 +1,15 @@
+package com.stackandqueue;
+
+public class StackQueueMain {
+
+	public static void main(String[] args) {
+
+		Stack stack = new Stack();
+
+		stack.push(70);
+		stack.push(30);
+		stack.push(56);
+	}
+
+
+}
